fd15337746ffcdb199898f5c8b02314bcfb82b7b
Jure Bajic <Jure.Bajic@partner.bmw.de>
2020-03-10 14:18:01 +0100

    Add black formater

--- functions/formaters.bash
+++ functions/formaters.bash
@@ -3,3 +3,8 @@
 function _clang_diff_formater() {
     git --no-pager diff master...HEAD --name-only | paste -sd " " | xargs clang-format-6.0 -i -verbose -style=file
 }
+
+function _black_formatter()
+{
+    black --line-length=120 $@
+}
