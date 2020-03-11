0861aa29e9fa2a85dac81fb43c61a87b21f52cf0
Jure Bajic <Jure.Bajic@partner.bmw.de>
2020-03-11 09:19:53 +0100

    Add clang all formater

--- functions/formaters.bash
+++ functions/formaters.bash
@@ -1,5 +1,9 @@
 #/bin/bash
 
+function _clang_all_formater() {
+    find . -type f -regex ".*\.[cpp,hpp]+" -print | paste -sd " " | xargs clang-format-6.0 -i -verbose -style=file
+}
+
 function _clang_diff_formater() {
     git --no-pager diff master...HEAD --name-only | paste -sd " " | xargs clang-format-6.0 -i -verbose -style=file
 }
