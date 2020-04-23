ae1fa5f22f6661368a57a37d4d82482168999081
Jure Bajic <Jure.Bajic@partner.bmw.de>
2020-04-23 08:35:17 +0200

    Change ad alias

--- setup_git.bash
+++ setup_git.bash
@@ -2,7 +2,7 @@ function _setup_git() {
     echo "Setup git aliases!"
     git config --global alias.ch checkout
     git config --global alias.st status
-    git config --global alias.ad "add ."
+    git config --global alias.ad add
     git config --global alias.hist "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short"
     git config --global alias.destroy "!git checkout . && git reset HEAD"
     git config --global alias.annihilate "!git checkout . && git reset HEAD --hard && git clean -fdx"
