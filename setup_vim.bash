70d80cb9deaffeeb24c10171c0a6f6eb526207a2
Jure Bajic <jure.bajic@memgraph.io>
2021-05-24 12:21:08 +0200

    Fix vim

--- setup_vim.bash
+++ setup_vim.bash
@@ -1,6 +1,7 @@
 
 function _setup_vim() {
     echo "Setting up vim!"
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     mkdir -p ~/.vim/undodir
     cp vim/vimrc ~/.vimrc
     vim +'PlugInstall --sync' +qa
