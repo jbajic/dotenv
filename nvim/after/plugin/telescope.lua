3f92aa0a4e486c723c37410ca12e7f9ec09ef0e7
Jure Bajic <jure.bajic94@gmail.com>
2023-05-01 22:40:16 +0200

    Update conf

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,7 +5,10 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") }); 
 end)
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fs', require("telescope").extensions.live_grep_args.live_grep_args, { noremap = true }) 
+
+-- Without preview 
+vim.keymap.set('n', '<leader>fg', "<CMD>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<CR>", { noremap = true }) 
 -- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
--- nvim/lua/jbajic/packer.lua
+++ nvim/lua/jbajic/packer.lua
@@ -1,4 +1,20 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
+end
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -9,8 +25,13 @@ return require('packer').startup(function(use)
 
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+	  requires = { 
+		  {'nvim-lua/plenary.nvim'},
+		  {'nvim-telescope/telescope-live-grep-args.nvim'},
+	  },
+	  config = function() 
+	      require("telescope").load_extension("live_grep_args")
+      end
   }
 
   use({
@@ -40,11 +61,21 @@ return require('packer').startup(function(use)
 		  {'saadparwaiz1/cmp_luasnip'},
 		  {'hrsh7th/cmp-nvim-lsp'},
 		  {'hrsh7th/cmp-nvim-lua'},
-
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
 	  }
   }
+	  -- Commenting and Uncommenting plugin
+  use('preservim/nerdcommenter')
+
+	  -- Snippets
+  use('L3MON4D3/LuaSnip')
+
+	-- Git
+  use { "lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" }
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+		require("packer").sync()
+  end
 end)
 
