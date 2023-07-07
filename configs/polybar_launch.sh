fcecf83877d52f69d40a911a5bd3d6fef7517aef
Jure Bajic <jure.bajic94@gmail.com>
2023-07-07 11:18:01 +0200

    Fix setup

--- configs/polybar_launch.sh
+++ configs/polybar_launch.sh
@@ -4,7 +4,7 @@ name=main
 pkill polybar
 if type "xrandr"; then
   for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=${monitor} polybar --reload ${name}&
+    MONITOR=${monitor} polybar --config=~/.config/polybar/config.ini --reload ${name}&
   done
 else
   polybar --reload ${name} &
--- setup_vim.bash
+++ setup_vim.bash
@@ -1,8 +1,21 @@
 function _setup_neovim() {
     echo "Setting up neovim!"
     sudo apt update
-    sudo apt install -y neovim
+    sudo apt install curl -y
+    mkdir -p ~/.local/bin
+    pushd ~/.local/bin/
+      curl -LO  https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+      chmod u+x nvim.appimage
+      mv nvim.appimage nvim
+    popd
     cp -r nvim_new ~/.config/nvim/
+
+    pushd ~
+      git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+      cd nerd-fonts
+      ./install
+    popd
+
     _command_finished
 }
 
