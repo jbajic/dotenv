3f9523710787476e1fa82b88cbd9979bd5d812f8
Jure Bajic <jure.bajic94@gmail.com>
2024-11-11 15:02:08 +0100

    Change global editor for git

--- setup_git.bash
+++ setup_git.bash
@@ -13,6 +13,8 @@ function _setup_git() {
     git config --global alias.annihilate "!git checkout . && git reset HEAD --hard && git clean -fdx"
     git config --global alias.lg1 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)'"
     git config --global alias.lg2 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
+
+    git config --global core.editor nvim
     echo
     echo "Setup global git ignore!"
     cp configs/gitignore_global ~/.gitignore_global
